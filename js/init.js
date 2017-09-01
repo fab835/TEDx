9a2cb9f91a356869400e169a589e1b3691a7b83a
fab835 <fabioczamora@gmail.com>
2017-09-01 14:26:26 -0300

    first commit

---
+++ js/init.js
@@ -0,0 +1,29 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();   
+    $('.modal').modal();
+    $('ul.tabs').tabs();
+    $('.slider').slider();
+    $('.carousel').carousel({
+        padding: 200    
+	});
+	autoplay().delay(4500);
+
+    $('.target').pushpin({
+      top: 0,
+      bottom: 1000,
+      offset: 0
+    });
+
+    
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
